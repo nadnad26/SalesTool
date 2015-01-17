b78d12c191c59b4b6447f4bc60a0ec62adb01c4e
Nadja <Nadja@Nadja-PC>
2015-01-17 13:57:13 +0100

    First Change

---
+++ SalesTools/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+
+	}
+
+	private static void displayGreeting(){
+		System.out.println("Hello!");
+		System.out.println("How are you?");
+		
+		System.out.println("Test 1");
+	}
+}
---
+++ SalesTools/src/SalesData.java
@@ -0,0 +1,11 @@
+
+public class SalesData {
+
+	int data[] = {0, 3, 45};
+	
+	public void display(){
+		System.out.println("Display data!");
+	}
+	
+	
+}
